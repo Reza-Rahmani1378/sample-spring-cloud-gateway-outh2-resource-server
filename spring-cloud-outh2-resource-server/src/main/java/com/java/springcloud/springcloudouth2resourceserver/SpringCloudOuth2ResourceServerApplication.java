76d92525886d4e270b2bb7c6b39e9acd0942c76b
Reza <reza6273757@gmail.com>
2023-10-22 12:21:42 +0330

    Inintial Project Spring Cloud With Outh2 Resource Server

---
+++ spring-cloud-outh2-resource-server/src/main/java/com/java/springcloud/springcloudouth2resourceserver/SpringCloudOuth2ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.java.springcloud.springcloudouth2resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOuth2ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudOuth2ResourceServerApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-outh2-resource-server/src/test/java/com/java/springcloud/springcloudouth2resourceserver/SpringCloudOuth2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springcloud.springcloudouth2resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudOuth2ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
